bf6b7335158e668ad7e09fa968c2bdf1258386b6
rafaelwnk <rafaelcarmo749@gmail.com>
2025-05-22 16:55:44 -0300

    feat: add reset-password-page component

---
+++ src/app/pages/account/reset-password-page/reset-password-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reset-password-page',
+  imports: [],
+  templateUrl: './reset-password-page.component.html'
+})
+export class ResetPasswordPageComponent {
+
+}
