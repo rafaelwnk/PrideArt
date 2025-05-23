c131abb534e659a49e7dcfaf96f3ad52b628201b
rafaelwnk <rafaelcarmo749@gmail.com>
2025-05-22 23:11:27 -0300

    feat: add post-modal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./components/shared/navbar/navbar.component";
+import { PostModalComponent } from "./components/post-modal/post-modal.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
-  template: '<app-navbar></app-navbar>'
+  imports: [RouterOutlet],
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
 
