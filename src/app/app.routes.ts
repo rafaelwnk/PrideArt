2b45210ac223b13d9e8e154f3168b0ab204a1ddf
rafaelwnk <rafaelcarmo749@gmail.com>
2025-05-22 23:58:04 -0300

    feat: add home-page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { LoginPageComponent } from './pages/account/login-page/login-page.compon
 import { SignUpPageComponent } from './pages/account/sign-up-page/sign-up-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { FramePageComponent } from './pages/frame-page/frame-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: FramePageComponent
+        component: FramePageComponent,
+        children: [
+            { path: '', component: HomePageComponent}
+        ]
         
     },
     { path: 'login', component: LoginPageComponent },
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { PostModalComponent } from "../../post-modal/post-modal.component";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [PostModalComponent],
+  imports: [PostModalComponent, RouterModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
